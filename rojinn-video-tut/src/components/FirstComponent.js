f3378ea794e33f31a4c8f1f0656f6d1e0e2cca1c
nastike <subheshjoshi@yahoo.com>
2023-12-07 21:02:17 +0545

    Added USE STATE to change the age of the Student

--- rojinn-video-tut/src/components/FirstComponent.js
+++ rojinn-video-tut/src/components/FirstComponent.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
-const FirstComponent = ({ name, courseName, subjects, institute, age }) => {
-  const [state, setState] = useState(false);
+const FirstComponent = ({
+  name,
+  courseName,
+  subjects,
+  institute,
+  age,
+  isStudent,
+}) => {
+  const [state, setState] = useState(isStudent);
+  const [count, setCount] = useState(age);
   return (
     <div id='mycomponent' style={{ color: state ? 'red' : 'green' }}>
       <p>Example</p>
       <h1>{name}</h1>
       <h2>{courseName}</h2>
       <p>
-        Hello my name is {name}, I study {courseName} at {institute}. I am {age}{' '}
-        years old. I am{state ? '' : ' not'} a student of following subjects:
+        Hello my name is {name}, I study {courseName} at {institute}. I am{' '}
+        {count} years old. I am{state ? '' : ' not'} a student of following
+        subjects:
       </p>
+      <button onClick={(e) => setCount(count - 1)}>-</button>
       <button onClick={() => setState(!state)}>ChangeStudentStatus</button>
+      <button onClick={(e) => setCount(count + 1)}>+</button>
       <ul>
         {subjects.map((s) => (
           <li key={s}>{s}</li>
