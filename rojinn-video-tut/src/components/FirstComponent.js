f8d84ed6f9ce89b33fcd272ab4fef2d1fba02604
nastike <subheshjoshi@yahoo.com>
2023-12-07 14:59:14 +0545

    Using State and toggling using click events and changed background color

--- rojinn-video-tut/src/components/FirstComponent.js
+++ rojinn-video-tut/src/components/FirstComponent.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 const FirstComponent = ({ name, courseName, subjects, institute, age }) => {
-  const [a, b] = useState(false);
+  const [state, setState] = useState(false);
   return (
-    <div>
+    <div id='mycomponent' style={{ color: state ? 'red' : 'green' }}>
       <p>Example</p>
       <h1>{name}</h1>
       <h2>{courseName}</h2>
       <p>
         Hello my name is {name}, I study {courseName} at {institute}. I am {age}{' '}
-        years old. I am{a ? '' : ' not'} a student of following subjects:
+        years old. I am{state ? '' : ' not'} a student of following subjects:
       </p>
-      <button onClick={() => b(!a)}>ChangeStudentStatus</button>
+      <button onClick={() => setState(!state)}>ChangeStudentStatus</button>
       <ul>
         {subjects.map((s) => (
           <li key={s}>{s}</li>
