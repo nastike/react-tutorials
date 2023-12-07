0b78d13efedd27022a652b9783d87c58c221260e
nastike <subheshjoshi@yahoo.com>
2023-12-07 12:40:12 +0545

    Recommit

--- rojinn-video-tut/src/App.js
+++ rojinn-video-tut/src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 import FirstComponent from './components/FirstComponent';
 
 function App() {
-  return <FirstComponent name='Rojinn' courseName='MERN' />;
+  return (
+    <div className='App'>
+      <FirstComponent
+        name='Rojinn'
+        courseName='MERN'
+        age={23}
+        isStudent={true}
+        details={{
+          name: 'abcd',
+          bloodGroup: 'O+',
+          address: 'Kathmandu-24',
+        }}
+        subjects={['MondoDB', 'ExpressJS', 'ReactJS', 'NodeJS']}
+      />
+    </div>
+  );
 }
 
 export default App;
--- rojinn-video-tut/src/components/FirstComponent.js
+++ rojinn-video-tut/src/components/FirstComponent.js
@@ -4,6 +4,13 @@ const FirstComponent = (abc) => {
       <p>Example</p>
       <h1>{abc.name}</h1>
       <h2>{abc.courseName}</h2>
+      <ul>
+        <li>{abc.subjects[0]}</li>
+        <li>{abc.subjects[1]}</li>
+        <li>{abc.subjects[2]}</li>
+        <li>{abc.subjects[3]}</li>
+        <li>{abc.subjects[4]}</li>
+      </ul>
     </div>
   );
 };
