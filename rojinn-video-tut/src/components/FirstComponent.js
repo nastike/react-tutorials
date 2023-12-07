d82531b1ad2a59cd7f4f2cd06112e486149788a0
nastike <subheshjoshi@yahoo.com>
2023-12-07 12:01:02 +0545

    Recommit

---
+++ rojinn-video-tut/src/components/FirstComponent.js
@@ -0,0 +1,10 @@
+const FirstComponent = (abc) => {
+  return (
+    <div>
+      <p>Example</p>
+      <h1>{abc.name}</h1>
+      <h2>{abc.courseName}</h2>
+    </div>
+  );
+};
+export default FirstComponent;
